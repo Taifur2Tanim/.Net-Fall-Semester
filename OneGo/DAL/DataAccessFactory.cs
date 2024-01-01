761ce3e2b184d7e212a755dd630d9c992acc4475
Taifur2Tanaim <taifurmohammadt@gmail.com>
2024-01-01 11:08:07 +0600

    OneGo project my part

---
+++ OneGo/DAL/DataAccessFactory.cs
@@ -0,0 +1,29 @@
+﻿using DAL.Interfaces;
+using DAL.Models;
+using DAL.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DataAccessFactory
+    {
+        public static IRepo<User, string, User> UserData()
+        {
+            return new UserRepo();
+        }
+
+        public static IRepo<Hotel, int, Hotel> HotelData()
+        {
+            return new HotelRepo();
+        }
+
+        public static IRepo<WishProduct, int, WishProduct> WishProductData()
+        {
+            return new WishProductRepo();
+        }
+    }
+}
---
+++ OneGo/DAL/Migrations/202312312315201_initDB.cs
@@ -0,0 +1,63 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initDB : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Hotels",
+                c => new
+                    {
+                        HotelID = c.Int(nullable: false, identity: true),
+                        HotelName = c.String(nullable: false),
+                        Location = c.String(nullable: false),
+                        Description = c.String(nullable: false),
+                        ContactNumber = c.String(nullable: false),
+                        Email = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.HotelID);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserName = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 20),
+                        Email = c.String(nullable: false),
+                        Contact = c.String(nullable: false),
+                        Password = c.String(nullable: false, maxLength: 20),
+                        Age = c.Int(nullable: false),
+                        Gender = c.String(nullable: false),
+                        Profession = c.String(nullable: false),
+                        Type = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.UserName);
+            
+            CreateTable(
+                "dbo.WishProducts",
+                c => new
+                    {
+                        WishlistID = c.Int(nullable: false, identity: true),
+                        WishProductName = c.String(nullable: false),
+                        WishProductDescription = c.String(nullable: false),
+                        AskedBy = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.WishlistID)
+                .ForeignKey("dbo.Users", t => t.AskedBy)
+                .Index(t => t.AskedBy);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.WishProducts", "AskedBy", "dbo.Users");
+            DropIndex("dbo.WishProducts", new[] { "AskedBy" });
+            DropTable("dbo.WishProducts");
+            DropTable("dbo.Users");
+            DropTable("dbo.Hotels");
+        }
+    }
+}
